43668b0fcdc39a4cdf65dda087d6ef6cafd6170b
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 03:08:03 +0700

    feat(api): wire module base infrastructure

--- server/Rental-Manager-Web-App/src/RentalManager.Api/Program.cs
+++ server/Rental-Manager-Web-App/src/RentalManager.Api/Program.cs
@@ -1,23 +1,28 @@
+using RentalManager.Api.DependencyInjection;
+using RentalManager.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddApplicationModules(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseMiddleware<TenantResolutionMiddleware>();
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program;
