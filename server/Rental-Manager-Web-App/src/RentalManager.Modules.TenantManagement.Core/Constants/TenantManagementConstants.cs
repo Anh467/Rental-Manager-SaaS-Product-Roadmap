4f4b43760c08db675b0b80ee37207204b0c2cfb7
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 03:02:44 +0700

    feat(core): add tenant module constants

---
+++ server/Rental-Manager-Web-App/src/RentalManager.Modules.TenantManagement.Core/Constants/TenantManagementConstants.cs
@@ -0,0 +1,12 @@
+namespace RentalManager.Modules.TenantManagement.Core.Constants;
+
+public static class TenantManagementConstants
+{
+    public const string ModuleName = "TenantManagement";
+    public const string DatabaseSchema = "org";
+
+    public static class Tables
+    {
+        public const string Tenant = "Tenant";
+    }
+}
