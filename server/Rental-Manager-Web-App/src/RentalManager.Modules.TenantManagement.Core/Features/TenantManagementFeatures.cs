096dc97e9f0ac25e5b504651e7b340255c6c3085
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 03:02:50 +0700

    feat(core): add feature names

---
+++ server/Rental-Manager-Web-App/src/RentalManager.Modules.TenantManagement.Core/Features/TenantManagementFeatures.cs
@@ -0,0 +1,8 @@
+namespace RentalManager.Modules.TenantManagement.Core.Features;
+
+public static class TenantManagementFeatures
+{
+    public const string TenantProvisioning = "tenant-management.provisioning";
+    public const string TenantSuspension = "tenant-management.suspension";
+    public const string TenantConfiguration = "tenant-management.configuration";
+}
