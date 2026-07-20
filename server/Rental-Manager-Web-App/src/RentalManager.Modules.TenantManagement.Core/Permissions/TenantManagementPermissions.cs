7e58b3697f095b8b3344455405fd8770e3582283
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 02:53:48 +0700

    feat(core): add tenant permissions

---
+++ server/Rental-Manager-Web-App/src/RentalManager.Modules.TenantManagement.Core/Permissions/TenantManagementPermissions.cs
@@ -0,0 +1,9 @@
+namespace RentalManager.Modules.TenantManagement.Core.Permissions;
+
+public static class TenantManagementPermissions
+{
+    public const string View = "tenant-management.view";
+    public const string Create = "tenant-management.create";
+    public const string Update = "tenant-management.update";
+    public const string Delete = "tenant-management.delete";
+}
