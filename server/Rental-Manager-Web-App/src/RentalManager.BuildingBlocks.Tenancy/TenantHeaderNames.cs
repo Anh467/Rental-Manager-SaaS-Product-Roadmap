b038cb73fa3323036773a708e570aa4dfb936ea0
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 03:06:31 +0700

    feat(tenancy): add tenant header constants

---
+++ server/Rental-Manager-Web-App/src/RentalManager.BuildingBlocks.Tenancy/TenantHeaderNames.cs
@@ -0,0 +1,7 @@
+namespace RentalManager.BuildingBlocks.Tenancy;
+
+public static class TenantHeaderNames
+{
+    public const string TenantId = "X-Tenant-Id";
+    public const string TenantCode = "X-Tenant-Code";
+}
