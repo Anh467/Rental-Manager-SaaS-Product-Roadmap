4aa1a52dfda46f40927e3ee44c394988b3ac4738
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 03:10:51 +0700

    fix(api): register authentication and authorization services

--- server/Rental-Manager-Web-App/src/RentalManager.Api/Program.cs
+++ server/Rental-Manager-Web-App/src/RentalManager.Api/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 builder.Services.AddApplicationModules(builder.Configuration);
 
 var app = builder.Build();
