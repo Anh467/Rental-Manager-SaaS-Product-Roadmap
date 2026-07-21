bddc4290eec866f65ef934d2fab3dfff206a8f19
Nguyen Anh Viet <van123872000@gmail.com>
2026-07-21 23:06:25 +0700

    wire field repository to SQL connection factory

--- server/Rental-Manager-Web-App/src/RentalManager.Modules.TenantManagement.Infrastructure/Persistence/Repositories/FieldRepository.cs
+++ server/Rental-Manager-Web-App/src/RentalManager.Modules.TenantManagement.Infrastructure/Persistence/Repositories/FieldRepository.cs
@@ -1,5 +1,6 @@
 using RentalManager.Modules.TenantManagement.Application.Abstractions.Persistence;
 using RentalManager.Modules.TenantManagement.Domain.Entities.Dbo;
+using RentalManager.Modules.TenantManagement.Infrastructure.Persistence.Connections;
 using RentalManager.Modules.TenantManagement.Infrastructure.Persistence.Repositories.Common;
 
 namespace RentalManager.Modules.TenantManagement.Infrastructure.Persistence.Repositories;
@@ -8,4 +9,8 @@ internal sealed class FieldRepository :
     BaseEntityAuditRepository<Field, Guid>,
     IFieldRepository
 {
+    public FieldRepository(ISqlConnectionFactory connectionFactory)
+        : base(connectionFactory)
+    {
+    }
 }
